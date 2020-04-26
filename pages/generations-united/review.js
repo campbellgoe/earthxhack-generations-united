5c99b8cf2286278c13d445cbb5aac7f9e545c7ac
IDICX\gypt.minierva <gypt.minierva@santaferelo.com>
2020-04-27 07:27:36 +0800

    Review update

--- pages/generations-united/review.js
+++ pages/generations-united/review.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import Layout from '../../components/Layout'
 import StateFarmCard from '../../components/StateFarmCard'
 import Img from '../../components/Img'
+import { TextButton, BlockButton } from '../../components/Button'
 
 const Medal = '/images/medal.png'
 const Star = '/images/star.png'
@@ -35,6 +36,15 @@ const Review = styled(({ className = '' }) => (
         <Img src={Star} alt="Star" className="Star"/>
       </div>            
     </div>
+    <br/>
+    <h3><a href="url">TELL US MORE</a></h3>
+    <br/>
+    <br/>
+    <h4>Call Another Senior?</h4>
+    <div className="buttons-container">
+        <BlockButton>NO</BlockButton>
+        <BlockButton  className="Yes">YES</BlockButton>
+      </div>    
     </StateFarmCard>
   </Layout>
 ))`
@@ -50,6 +60,10 @@ const Review = styled(({ className = '' }) => (
   }
   .Star{
     width: 50%;
+  }
+  .Yes{
+    background-color: white;
+    color: red;
   }
   h1{
     
@@ -73,6 +87,30 @@ const Review = styled(({ className = '' }) => (
     color: #FFFFFF;
 
   }
+  
+  h3{
+
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 16px;
+    text-align: center;
+    color: #FFFFFF;
+
+  }
+  a{
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 14px;
+    text-align: center;
+    text-decoration-line: underline;
+    text-transform: uppercase;
+
+    color: #FFFFFF;
+  }
   .column {
 
     float: left;
@@ -83,6 +121,14 @@ const Review = styled(({ className = '' }) => (
     content: "";
     clear: both;
     display: table;
+  }  
+  .buttons-container {
+    display: flex;
+    justify-content: space-between;
+    button {
+      width: calc(50% - 8px);
+      min-width: 120px;
+    }
   }  
   /* put your styles here, it is similar to CSS */
 `
