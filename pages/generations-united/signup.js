be619ea99ac2bf648753d6e1c4a6cc3f1f71d8c8
IDICX\gypt.minierva <gypt.minierva@santaferelo.com>
2020-04-27 04:20:49 +0800

    Sign up update

--- pages/generations-united/signup.js
+++ pages/generations-united/signup.js
@@ -6,24 +6,22 @@ import StateFarmCard from '../../components/StateFarmCard'
 
 import Img from '../../components/Img'
 
-const TechSupportImage = '/images/elderly-tech-support.jpeg'
+const TechSupportImage = '/images/young-elderly.png'
 
 const SignUpPage = styled(({ className = '' }) => {
   //jsx goes here (see JSX syntax)
   return (<Layout className={className} title={'Green Youth Ambassador - Sign up'}>
     <StateFarmCard className="StateFarmCard">
       <Img src={TechSupportImage} alt="tech support for the elderly" className="rounded" />
-      <h3>Green Youth Ambassador</h3>
-      <p>Lower your insurance by sharing your tech knowledge.</p>
-      <p>Less tech savvy people are particularly vulnerable during times of crisis.
-      Without access to a smartphone or computer, they are missing out on
-        communicating with loved ones with modern technology.</p>
-      <p>You can help these folk learn technology such as that offered in our
-        Drive Safe & Save program whilst earning insurance credits, a win-win situation.</p>
+      <h3>Generations United</h3>
+      
+      <p>During this unprecedented crisis, social isolation particularly for the elderly communities is at an all time high. Help connect to elderly StateFarm members and receive a discount on your insurance for doing so. </p>
+      <p>Use this portal to sign-up, call and check-in with your neighbors. The portal is live during business hours and will connect you via landline to an elderly person that would like to chat. </p>
+      <p>At any time during the call, if you are concerned for this persons' mental or physical health, you can flag their profile to connect with local and state experts for further services.</p>
       <div className="buttons-container">
         <TextButton>LEARN MORE</TextButton>
         <Link href="/generations-united/languages">
-          <BlockButton>JOIN NOW</BlockButton>
+          <BlockButton>SIGN UP</BlockButton>
         </Link>
       </div>
     </StateFarmCard>
